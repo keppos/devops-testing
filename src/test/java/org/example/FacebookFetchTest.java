9ecff48e07a5e979647f809145948e0efff27f16
keppos <kevin.osterberg@hotmail.com>
2023-10-10 15:01:48 +0300

    final version

--- src/test/java/org/example/FacebookFetchTest.java
+++ src/test/java/org/example/FacebookFetchTest.java
@@ -2,19 +2,31 @@ package org.example;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
 public class FacebookFetchTest {
 
     @Test
-    public void testGetFriends_validLink() {
-        FacebookFetch parser = new FacebookFetch();
-        String[] friends = parser.getFriends("https://www.facebook.com/kevin_osterberg");
-        Assert.assertArrayEquals(new String[]{"Friend1", "Friend2", "Friend3"}, friends);
+    public void testRetrieveFriends_Positive() {
+        FacebookFetch retriever = new FacebookFetch();
+        String[] expected = {"Friend1", "Friend2", "Friend3"};
+        String[] actual = retriever.getFriends("facebook.com/kevin_osterberg");
+        assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testGetFriends_invalidLink() {
-        FacebookFetch parser = new FacebookFetch();
-        String[] friends = parser.getFriends("invalidLink");
-        Assert.assertNull(friends); // Assuming null is returned for an invalid link
+    public void testRetrieveFriends_EmptyInput() {
+        FacebookFetch retriever = new FacebookFetch();
+        String[] expected = {};
+        String[] actual = retriever.getFriends("");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testRetrieveFriends_NullInput() {
+        FacebookFetch retriever = new FacebookFetch();
+        String[] expected = {};
+        String[] actual = retriever.getFriends(null);
+        assertArrayEquals(expected, actual);
     }
 }
